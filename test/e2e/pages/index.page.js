8a6425a5b002599b77e23f99b91f171ce518039e
Daniel Cottone <daniel.cottone@gmail.com>
2016-06-25 14:33:47 -0500

    additional protractor tests

--- test/e2e/pages/index.page.js
+++ test/e2e/pages/index.page.js
@@ -5,6 +5,7 @@ var IndexPage = function () {
   this.cityInput = element(by.model('city'));
   this.currentWeatherDiv = element(by.id('current-weather'));
   this.forecastDivs = element.all(by.id('forecast-day'));
+  this.body = element(by.css('body'));
 
   this.visit = function () {
     browser.get(this.path);
--- test/e2e/specs/index.spec.js
+++ test/e2e/specs/index.spec.js
@@ -11,12 +11,16 @@ describe('BeautifulWeather: Index', function () {
   });
 
   it('should display properly on page load', function () {
-    expect(page.cityInput.isDisplayed()).toEqual(true);
-    expect(page.currentWeatherDiv.isDisplayed()).toEqual(false);
+    expect(page.cityInput.isDisplayed()).toBe(true);
+    expect(page.currentWeatherDiv.isDisplayed()).toBe(false);
+    expect(page.forecastDivs.count()).toBe(0);
+    expect(page.body.getAttribute('class')).toBe('ng-scope');
   });
 
   it('should display weather information', function () {
     page.setCityInput('Nashville');
-    expect(page.currentWeatherDiv.isDisplayed()).toEqual(true);
+    expect(page.currentWeatherDiv.isDisplayed()).toBe(true);
+    expect(page.forecastDivs.count()).toBe(7);
+    expect(page.body.getAttribute('class')).not.toBe('ng-scope');
   });
 });
